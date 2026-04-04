c69ce152bc6578a07d0dcf96383e84e0299b505d
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2026-04-04 08:24:12 +0700

    Add search vector index script

--- postgres.init.sql
+++ postgres.init.sql
@@ -1,1 +1,5 @@
 CREATE EXTENSION IF NOT EXISTS postgis;
+
+-- after migration, run the following command to update the search_vector column for existing data
+-- CREATE INDEX idx_reports_search_vector
+-- ON reports USING GIN (search_vector);
