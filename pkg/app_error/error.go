6af89e00451fd26e5dc3822218db33289bb33387
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2026-07-21 20:02:55 +0700

    Add error data field

--- pkg/app_error/error.go
+++ pkg/app_error/error.go
@@ -4,18 +4,20 @@ type AppError struct {
     Code       string `json:"code"`
     Message    string `json:"message"`
     Details     string `json:"details,omitempty"`
-    StatusCode int    `json:"statusCode"`
+    StatusCode int    `json:"status_code"`
+    ErrorData   any `json:"error_data,omitempty"`
 }
 
 func (e *AppError) Error() string {
     return e.Message
 }
 
-func New(status int, code, message string, details string) *AppError {
+func New(status int, code, message string, details string, errorData any) *AppError {
     return &AppError{
         Code:       code,
         Message:    message,
         Details:    details,
         StatusCode: status,
+        ErrorData:  errorData,
     }
 }
