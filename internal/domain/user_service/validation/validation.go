1410a983563205e196a67483a20f8096d35be82f
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2026-07-26 16:08:22 +0700

    Add get follow data validation

--- internal/domain/user_service/validation/validation.go
+++ internal/domain/user_service/validation/validation.go
@@ -107,3 +107,26 @@ func FormatFollowValidationErrors(err error) map[string]string {
 	}
 	return errors
 }
+
+func FormatGetFollowDataValidationErrors(err error) map[string]string {
+	errors := map[string]string{}
+	if err == nil {
+		return errors
+	}
+	for _, e := range err.(validator.ValidationErrors) {
+		switch e.Field() {
+		case "FollowingID":
+			if e.Tag() == "required" {
+				errors["followingID"] = "ID yang diikuti wajib diisi"
+			}
+		case "FollowingType":
+			if e.Tag() == "required" {
+				errors["followingType"] = "Tipe yang diikuti wajib diisi"
+			}
+			if e.Tag() == "oneof" {
+				errors["followingType"] = "Tipe yang diikuti harus salah satu antara pengguna atau komunitas"
+			}
+		}
+	}
+	return errors
+}
