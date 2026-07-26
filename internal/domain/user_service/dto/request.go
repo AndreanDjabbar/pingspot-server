f8f1cc615a30b2b58f958291bcea06427fd9363f
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2026-07-26 16:04:24 +0700

    Add get follow data request and response DTO

--- internal/domain/user_service/dto/request.go
+++ internal/domain/user_service/dto/request.go
@@ -20,3 +20,8 @@ type FollowRequest struct {
 	FollowingID   uint   `json:"followingID" validate:"required"`
 	FollowingType string `json:"followingType" validate:"required,oneof=user community"`
 }
+
+type GetFollowDataRequest struct {
+	FollowingID   uint   `json:"followingID" validate:"required"`
+	FollowingType string `json:"followingType" validate:"required,oneof=user community"`
+}
--- internal/domain/user_service/dto/response.go
+++ internal/domain/user_service/dto/response.go
@@ -37,6 +37,12 @@ type SearchResponse struct {
 type FollowResponse struct {
 	FollowingID   uint   `json:"followingID"`
 	FollowingType string `json:"followingType"`
-	FollowerUserID  uint   `json:"followerUserId"`
+	FollowerUserID  uint   `json:"followerUserID"`
 	FollowProcess string `json:"followProcess"`
+}
+
+type GetFollowDataResponse struct {
+	FollowingID   uint   `json:"followingID"`
+	FollowersCount int64  `json:"followersCount"`
+	FollowingCount int64  `json:"followingCount"`
 }
