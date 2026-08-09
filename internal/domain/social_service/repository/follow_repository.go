5b9217654017dd22131713497c6a9c5154df57ff
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2026-08-09 19:24:49 +0700

    Add get followers and following repository

--- internal/domain/social_service/repository/follow_repository.go
+++ internal/domain/social_service/repository/follow_repository.go
@@ -13,6 +13,8 @@ type FollowRepository interface {
 	DeleteTX(ctx context.Context, tx *gorm.DB, follow *model.Follow) error
 	GetFollowersCount(ctx context.Context, followingID uint, followingType model.FollowingType) (int64, error)
 	GetFollowingCount(ctx context.Context, followerUserID uint, followingType model.FollowingType) (int64, error)
+	GetFollowersByUserID(ctx context.Context, userID uint) ([]*model.User, error)
+	GetFollowingByUserID(ctx context.Context, userID uint) ([]*model.User, error)
 }
 
 type followRepository struct {
@@ -59,4 +61,34 @@ func (r *followRepository) GetFollowingCount(ctx context.Context, followerUserID
 		return 0, err
 	}
 	return count, nil
+}
+
+func (r *followRepository) GetFollowersByUserID(ctx context.Context, userID uint) ([]*model.User, error) {
+	var followers []*model.User
+
+    if err := r.db.WithContext(ctx).
+        Model(&model.User{}).
+        Preload("Profile").
+        Joins("JOIN follows ON follows.follower_user_id = users.id").
+        Where("follows.following_id = ? AND follows.following_type = ?", userID, model.FollowingTypeUser).
+        Find(&followers).Error; err != nil {
+        return nil, err
+    }
+
+    return followers, nil
+}
+
+func (r *followRepository) GetFollowingByUserID(ctx context.Context, userID uint) ([]*model.User, error) {
+	var following []*model.User
+
+    if err := r.db.WithContext(ctx).
+        Model(&model.User{}).
+        Preload("Profile").
+        Joins("JOIN follows ON follows.following_id = users.id").
+        Where("follows.follower_user_id = ? AND follows.following_type = ?", userID, model.FollowingTypeUser).
+        Find(&following).Error; err != nil {
+        return nil, err
+    }
+
+    return following, nil
 }
