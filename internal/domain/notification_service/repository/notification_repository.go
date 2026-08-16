23baa8b24e0adf7addd23ab2f749d5fc2851d801
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2026-08-16 16:44:27 +0700

    Add notification repository

---
+++ internal/domain/notification_service/repository/notification_repository.go
@@ -0,0 +1,36 @@
+package repository
+
+import (
+	"context"
+	"pingspot/internal/model"
+
+	"gorm.io/gorm"
+)
+
+type NotificationRepository interface {
+	GetByUserID(ctx context.Context, userID uint) (*[]model.Notification, error)
+	CreateTX(ctx context.Context, tx *gorm.DB, notification *model.Notification) error
+}
+
+type notificationRepository struct {
+	db *gorm.DB
+}
+
+func NewNotificationRepository(db *gorm.DB) NotificationRepository {
+	return &notificationRepository{db: db}
+}
+
+func (r *notificationRepository) GetByUserID(ctx context.Context, userID uint) (*[]model.Notification, error) {
+	var notifications []model.Notification
+	if err := r.db.WithContext(ctx).Where("user_id = ?", userID).Find(&notifications).Error; err != nil {
+		return nil, err
+	}
+	return &notifications, nil
+}
+
+func (r *notificationRepository) CreateTX(ctx context.Context, tx *gorm.DB, notification *model.Notification) error {
+	if err := tx.WithContext(ctx).Create(notification).Error; err != nil {
+		return err
+	}
+	return nil
+}
