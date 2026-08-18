ecf97bf418d2a5db61b3cbe2cc0965c979c587f8
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2026-08-18 20:18:35 +0700

    Add task service to the social service

--- internal/domain/social_service/router/router.go
+++ internal/domain/social_service/router/router.go
@@ -1,23 +1,29 @@
 package router
 
 import (
+	"fmt"
 	"pingspot/internal/domain/social_service/handler"
+	socialRepository "pingspot/internal/domain/social_service/repository"
 	"pingspot/internal/domain/social_service/service"
 	userRepository "pingspot/internal/domain/user_service/repository"
-	socialRepository "pingspot/internal/domain/social_service/repository"
 	"pingspot/internal/infrastructure/database"
 	"pingspot/internal/middleware"
+	taskService "pingspot/internal/domain/task_service/service"
+	env "pingspot/pkg/utils/env_util"
 	"time"
-
 	"github.com/gofiber/fiber/v2"
+	"github.com/hibiken/asynq"
 )
 
 func RegisterSocialRoutes(app *fiber.App) {
 	db := database.GetPostgresDB()
 	userRepo := userRepository.NewUserRepository(db)
 	followRepo := socialRepository.NewFollowRepository(db)
+	redisAddress := fmt.Sprintf("%s:%s", env.RedisHost(), env.RedisPort())
+	client := asynq.NewClient(asynq.RedisClientOpt{Addr: redisAddress})
+	tasksService := taskService.NewTaskService(client)
 	
-	socialService := service.NewSocialService(db, followRepo, userRepo)
+	socialService := service.NewSocialService(db, followRepo, userRepo, tasksService)
 	socialHandler := handler.NewSocialHandler(socialService)
 
 	followRoute := app.Group("/pingspot/api/social/follow", middleware.ValidateAccessToken())
